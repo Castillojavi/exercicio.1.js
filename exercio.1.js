3c7428568a562a4f5dfc144adf82ae6770c941c9
leonardo <105162646+Castillojavi@users.noreply.github.com>
2022-06-26 17:32:59 -0300

    ejecutando prompt-js

---
+++ exercio.1.js
@@ -0,0 +1,9 @@
+let nome = prompt ('qual é o seu nome?' , "leonardo");
+alert(`seu nome e  ${nome}!`);
+let sobrenome = prompt ('qual é o seu sobrenome?' , "castillo");
+alert(`seu sobrenome e  ${sobrenome}!`);
+let edade = prompt ('qual é o sua adade?' , "41");
+alert(`sua edade e  ${edade}!`);
+let height = prompt ('qual é sua altura?' , "1,76");
+alert(`sua altura e ${height}!`);
+
